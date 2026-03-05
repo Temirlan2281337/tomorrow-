407ada187dd0fb64d06c1ff8b715642d8cc7d172
Your Name <you@example.com>
2026-03-05 15:41:38 +0500

    asd

--- cameltosnakecase3.1.go
+++ cameltosnakecase3.1.go
@@ -1,3 +1,40 @@
 package main
 
+import (
+	"unicode"
+)
 
+func CamelToSnakeCase(s string) string {
+	if s == "" {
+		return ""
+	}
+
+	for i, r := range s {
+
+		// запрещены цифры и символы
+		if !unicode.IsLetter(r) {
+			return s
+		}
+
+		// нельзя две заглавные подряд
+		if i > 0 && unicode.IsUpper(r) && unicode.IsUpper(rune(s[i-1])) {
+			return s
+		}
+
+		// нельзя заканчивать на заглавную
+		if i == len(s)-1 && unicode.IsUpper(r) {
+			return s
+		}
+	}
+
+	result := ""
+
+	for i, r := range s {
+		if i > 0 && unicode.IsUpper(r) {
+			result += "_"
+		}
+		result += string(r)
+	}
+
+	return result
+}
